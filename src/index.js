d341e879deaca6f2ca3319f81a07e0a47093fe1c
jahanzaibshahid <jahanzaibshahid07@gmail.com>
2020-11-21 17:05:03 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// JSX fragment is used to avoid extra <div> in memory
+// {} is used JSX expression not for statement 
+
+// const fname  = "jahanzaib shahid";
+// ReactDom.render(
+//       <React.Fragment>
+//           <h1>my name is {fname} </h1>
+//           <h2> sum is {2+3} </h2>
+//           <h2> sum is {Math.random()} </h2>
+//       </React.Fragment>
+//      ,
+//      document.getElementById('root')
+// );
+
+// {`${} ${}`} it is called template literals 
+
+// contentEditable="true" JSX attribute 
+
+const fname = "Jahanzaib";
+const lname = "Shahid";
+const date  = new Date();
+
+ReactDom.render(
+      <React.Fragment>
+          <h2 contentEditable="true"> My Name Is {`${fname} ${lname}`}</h2>
+          <h2> Current Date {date.toLocaleDateString()} </h2>
+          <h2> Current Time {date.toLocaleTimeString()} </h2>
+      </React.Fragment>
+     ,
+     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
